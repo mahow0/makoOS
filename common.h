adc34a1c224b3edd1e7ef58a1cddb000492e5f09
Mahmoud <xquisitor@gmail.com>
2025-02-26 13:43:58 -0500

    feat: linear/bump allocator

--- common.h
+++ common.h
@@ -26,6 +26,8 @@ typedef uint32_t vaddr_t;
 #define va_end __builtin_va_end
 #define va_arg __builtin_va_arg
 
+// Page size is set to 4096
+#define PAGE_SIZE 4096
 
 // C standard library functions, we're redefining them here since our OS cannot depend on stdlib
 void *memset(void *buf, char c, size_t n);
--- kernel.c
+++ kernel.c
@@ -135,13 +135,34 @@ void handle_trap(struct trap_frame *f) {
     PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
 }
 
+// Memory management
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    // ``next_paddr`` -> beginning of newly unallocated space
+    // static keyword indicates that ``next_paddr`` retains its value across function calls
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+    
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 void kernel_main(void) {
 
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    WRITE_CSR(stvec, (uint32_t) kernel_entry);
-    //psudo-instruction that triggers an illegal instruction exception
-    __asm__ __volatile__("unimp");
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("__free_ram: %x\n", __free_ram);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+    PANIC("booted!");
 }
 
 
